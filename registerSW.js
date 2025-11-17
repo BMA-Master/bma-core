adbe595cd571eb881e870e1a0f587589ccafda65
neodigm <3151842+neodigm@users.noreply.github.com>
2025-11-16 18:04:53 -0600

    deploy dev bma_tournament_core arev

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bma-core/sw.js', { scope: '/bma-core/' })})}
